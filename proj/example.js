b4d180bea47d8c09967d93d86ae6a0acaec22339
Ayush M. <ashu.mau.3114@gmail.com>
2024-10-19 19:05:55 +0530

    Add files via upload

---
+++ proj/example.js
@@ -0,0 +1,10 @@
+function add(a, b) {
+    return a + b;
+}
+
+function addDuplicated(a, b) {
+    return a + b;
+}
+
+console.log(add(1, 2)); // Outputs: 3
+console.log(addDuplicated(1, 2)); // Outputs: 3
