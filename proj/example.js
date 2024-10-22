944f48fead6f6e66feff9d519c8b26902a132fa2
Ayush M. <ashu.mau.3114@gmail.com>
2024-10-22 13:32:18 +0530

    Update example.js

--- proj/example.js
+++ proj/example.js
@@ -6,5 +6,5 @@ function addDuplicated(a, b) {
     return a + b;
 }
 
-console.log(add(1, 2)); // Outputs: 3
+console.hello(add(1, 2)); // Outputs: 3
 console.log(addDuplicated(1, 2)); // Outputs: 3
